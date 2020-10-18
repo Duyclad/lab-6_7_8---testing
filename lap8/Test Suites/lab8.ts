1c748441d858b2e170dbaf18b4c59a49cc4b9a9c
duyclad <coleanhduy.study@gmail.com>
2020-10-18 23:54:27 +0700

    upfile

---
+++ lap8/Test Suites/lab8.ts
@@ -0,0 +1,71 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>lab8</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>96e39213-8d28-444f-9161-d2b0a908b0e4</testSuiteGuid>
+   <testCaseLink>
+      <guid>c11406f6-4a4d-4fd2-9fd5-6227b6324564</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC001</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>bf64b405-565a-4f4d-9fd2-84079b3117a8</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Lab8</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>bf64b405-565a-4f4d-9fd2-84079b3117a8</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>26143e77-8918-494b-a1f5-678fe6423805</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fc3435f0-77ec-410c-9e06-b41a0af4c4e3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC002</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>08975c08-453d-4568-8ddb-d45048505179</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e247edda-48d7-4cfa-a2ef-46a6d0510141</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC004</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>64a863b3-2f76-4c0f-9610-ffcdeeea13dd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC005</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d622ed11-fa6e-4135-bdf9-17ccb24b475e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC003</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>8ed23da0-47f1-4b2b-9097-0898a52a07ce</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
